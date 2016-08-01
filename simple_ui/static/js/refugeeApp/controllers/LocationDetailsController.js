5b9c45806283866855d9c402d07d7fd4a58c2d4e
Kamil Krampa <kkrampa@soldevelo.com>
2016-08-01 13:52:23 +0200

    Moved map to directive (#16)

--- simple_ui/static/js/refugeeApp/controllers/LocationDetailsController.js
+++ simple_ui/static/js/refugeeApp/controllers/LocationDetailsController.js
@@ -1,34 +1,11 @@
-angular.module('refugeeApp').controller('LocationDetailsController', function($scope, $stateParams, $state, djangoRMI, leafletData) {
+angular.module('refugeeApp').controller('LocationDetailsController', function($scope, $stateParams, $state, djangoRMI) {
     var vm = this;
     vm.data = {};
     vm.loaded = false;
 
-    angular.extend(vm, {
-        defaults: {
-            scrollWheelZoom: false
-        }
-    });
-
     djangoRMI.location_json_view.get_details({slug: $stateParams.slug}).success(function(data) {
         vm.data = data;
         vm.loaded = true;
-        angular.extend(vm, {
-            geojson: {
-                data: data.location.envelope,
-                style: {
-                    fillColor: "green",
-                    weight: 2,
-                    opacity: 1,
-                    color: 'white',
-                    dashArray: '3',
-                    fillOpacity: 0.7
-                }
-            }
-        });
-        var polygon = L.geoJson(vm.geojson.data);
-        leafletData.getMap().then(function(map) {
-            map.fitBounds(polygon.getBounds());
-        });
     });
 
     vm.navigateTo = function(state) {
---
+++ simple_ui/static/js/refugeeApp/directives/regionMap.js
@@ -0,0 +1,75 @@
+angular.module('refugeeApp').directive('regionMap', function(leafletData) {
+    return {
+        restrict: 'E',
+        scope: {
+            region: '=',
+            theme: '@?'
+        },
+        link: {
+            pre: function(scope) {
+                angular.extend(scope, {
+                    defaults: {
+                        scrollWheelZoom: false
+                    },
+                    tiles: {
+                        dark: {
+                            url: "http://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png",
+                            options: {
+                                attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                            }
+                        },
+                        light: {
+                            url: "http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png",
+                            options: {
+                                attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                            }
+                        }
+                    }
+                });
+
+                if (!scope.theme) {
+                    scope.theme = 'dark';
+                }
+                scope.tile = scope.tiles[scope.theme];
+            },
+            post: function(scope) {
+                var refreshMap = function() {
+                    angular.extend(scope, {
+                        geojson: {
+                            data: scope.region,
+                            style: {
+                                fillColor: "#48b04f",
+                                weight: 2,
+                                opacity: 1,
+                                color: 'white',
+                                fillOpacity: 0.3
+                            }
+                        }
+                    });
+                    var polygon = L.geoJson(scope.geojson.data);
+                    leafletData.getMap().then(function(map) {
+                        map.fitBounds(polygon.getBounds());
+                    });
+                };
+
+                scope.$watch('region', function (newValue, oldValue) {
+                    if (oldValue === newValue) {
+                        return;
+                    }
+                    scope.region = newValue;
+                    refreshMap();
+                }, true);
+
+                scope.$watch('theme', function (newValue, oldValue) {
+                    if (oldValue === newValue) {
+                        return;
+                    }
+
+                    scope.theme = newValue;
+                    refreshMap();
+                }, true);
+            }
+        },
+        template: '<leaflet geojson="geojson" tiles="tile" style="height: 400px"></leaflet>'
+    };
+});
