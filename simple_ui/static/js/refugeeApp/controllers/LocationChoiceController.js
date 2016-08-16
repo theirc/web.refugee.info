3ccf98d0ab08d7ab61b22a45e58048eaa123e981
Kamil Krampa <kkrampa@soldevelo.com>
2016-08-16 08:25:59 +0200

    Fixed closest location link

--- simple_ui/static/js/refugeeApp/controllers/LocationChoiceController.js
+++ simple_ui/static/js/refugeeApp/controllers/LocationChoiceController.js
@@ -1,6 +1,10 @@
-angular.module('refugeeApp').controller('LocationChoiceController', function($rootScope, locationData) {
+angular.module('refugeeApp').controller('LocationChoiceController', function($rootScope, $state, locationData) {
     var vm = this;
     vm.data = locationData;
 
     $rootScope.location = null;
+
+    vm.navigateTo = function(slug) {
+        $state.go('locationDetails.index', {slug: slug});
+    };
 });
