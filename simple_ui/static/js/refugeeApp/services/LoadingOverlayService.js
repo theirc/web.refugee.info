5265844809d2eaf20e35c461dc2715194f33deea
bklukaczewski <bklukaczewski@gmail.com>
2016-08-16 14:30:43 +0200

    41: Animated loading screen spinner

--- simple_ui/static/js/refugeeApp/services/LoadingOverlayService.js
+++ simple_ui/static/js/refugeeApp/services/LoadingOverlayService.js
@@ -1,9 +1,24 @@
 angular.module('refugeeApp').factory('LoadingOverlayService', function () {
+    var template = '\
+    <div class="sk-fading-circle"> \
+        <div class="sk-circle1 sk-circle"></div> \
+        <div class="sk-circle2 sk-circle"></div> \
+        <div class="sk-circle3 sk-circle"></div> \
+        <div class="sk-circle4 sk-circle"></div> \
+        <div class="sk-circle5 sk-circle"></div> \
+        <div class="sk-circle6 sk-circle"></div> \
+        <div class="sk-circle7 sk-circle"></div> \
+        <div class="sk-circle8 sk-circle"></div> \
+        <div class="sk-circle9 sk-circle"></div> \
+        <div class="sk-circle10 sk-circle"></div> \
+        <div class="sk-circle11 sk-circle"></div> \
+        <div class="sk-circle12 sk-circle"></div> \
+    </div>';
     return {
-        start: function() {
-            $.isLoading({ text: 'Loading...' });
+        start: function () {
+            $.isLoading({tpl: template});
         },
-        stop: function() {
+        stop: function () {
             $.isLoading('hide');
         }
     };
