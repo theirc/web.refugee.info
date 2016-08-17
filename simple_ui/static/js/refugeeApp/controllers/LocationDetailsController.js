8a1ef509e64494f6446e2f6638587be51d047e20
bklukaczewski <bklukaczewski@gmail.com>
2016-08-17 14:13:42 +0200

    39: Create important information page; fix important information language

--- simple_ui/static/js/refugeeApp/controllers/LocationDetailsController.js
+++ simple_ui/static/js/refugeeApp/controllers/LocationDetailsController.js
@@ -1,7 +1,18 @@
-angular.module('refugeeApp').controller('LocationDetailsController', function($scope, $stateParams, $state, djangoRMI, location) {
+angular.module('refugeeApp').controller('LocationDetailsController', function ($scope, $stateParams, $state, djangoRMI, location) {
     var vm = this;
     vm.data = {};
     vm.loaded = false;
     vm.slug = $stateParams.slug;
     vm.location = location;
+
+    vm.openImportantInfo = function (id) {
+        var section = $('#info-' + id);
+        if (!section.hasClass("hidden")) {
+            section.addClass("hidden");
+        }
+        else {
+            $('.important-information-content-container').addClass('hidden');
+            section.removeClass('hidden');
+        }
+    };
 });
