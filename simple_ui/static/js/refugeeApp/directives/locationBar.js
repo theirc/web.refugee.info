d47de0f18030f1e7bf5d56c8804a357775438abd
knowak <knowak@soldevelo.com>
2016-08-25 11:25:43 +0200

    Fix change location

--- simple_ui/static/js/refugeeApp/directives/locationBar.js
+++ simple_ui/static/js/refugeeApp/directives/locationBar.js
@@ -2,6 +2,12 @@ angular.module('refugeeApp').directive('locationBar', function () {
     return {
         restrict: 'E',
         scope: {},
-        templateUrl: '/partials/directives/location-bar.html'
+        templateUrl: '/partials/directives/location-bar.html',
+        controller: function($scope, $state, $cookies) {
+            $scope.changeLocation = function() {
+                $cookies.remove('locationSlug');
+                $state.go('location');
+            };
+        }
     };
 });
