78b8c5a2fccc426013945f57fdff67f39e1c985e
knowak <knowak@soldevelo.com>
2016-09-23 16:42:40 +0200

    Highlight chosen rating

--- simple_ui/static/js/refugeeApp/directives/ratingThumbs.js
+++ simple_ui/static/js/refugeeApp/directives/ratingThumbs.js
@@ -13,14 +13,17 @@ angular.module('refugeeApp').directive('ratingThumbs', function (LocationService
             $scope.$storage = $localStorage;
             vm.thumbs_up = 0;
             vm.thumbs_down = 0;
+            var ratingId = 'rating_' + location.slug + '_';
+            ratingId += vm.slug ? vm.slug : vm.index;
             LocationService.getRating(location, vm.index, vm.slug).then(function (response) {
                 vm.thumbs_up = response.data.thumbs_up;
                 vm.thumbs_down = response.data.thumbs_down;
+                vm.rating = $scope.$storage[ratingId];
             });
 
             vm.rate = function (rating) {
-                var ratingId = 'rating_' + location.slug + '_' + vm.slug ? vm.slug : vm.index;
                 var result = $scope.$storage[ratingId];
+                vm.rating = rating;
                 if (!result) {
                     LocationService.setRating($scope.$root.location, vm.index, vm.slug, rating).then(function (response) {
                         vm.thumbs_up = response.data.thumbs_up;
