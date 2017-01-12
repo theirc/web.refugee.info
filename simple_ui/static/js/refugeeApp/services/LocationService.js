32ce2bbf808e792fddbeaf66fbf72190a0c5327d
Artur Walkowiak <awalkowiak@soldevelo.com>
2017-01-12 14:37:03 +0100

    Adapt services in location

--- simple_ui/static/js/refugeeApp/services/LocationService.js
+++ simple_ui/static/js/refugeeApp/services/LocationService.js
@@ -2,14 +2,12 @@ angular.module('refugeeApp').factory('LocationService', function ($http, apiUrl,
     return {
         getServices: function (location, page, search, typeNumbers) {
             page = page || 1;
-            var coords = location.centroid.coordinates;
 
             return $http({
                 method: 'GET',
-                url: apiUrl + '/v1/services/search/',
+                url: apiUrl + '/v2/services/search/',
                 params: {
                     geographic_region: location.slug,
-                    closest: coords[0] + ',' + coords[1],
                     page: page,
                     page_size: 12,
                     search: search,
@@ -21,14 +19,14 @@ angular.module('refugeeApp').factory('LocationService', function ($http, apiUrl,
         getServiceTypes: function () {
             return $http({
                 method: 'GET',
-                url: apiUrl + '/v1/servicetypes/',
+                url: apiUrl + '/v2/servicetypes/',
                 headers: {'Accept-Language': $translate.proposedLanguage() || $translate.use()}
             });
         },
         getService: function (serviceId) {
             return $http({
                 method: 'GET',
-                url: apiUrl + '/v1/services/search/?format=json&id=' + serviceId,
+                url: apiUrl + '/v2/services/search/?format=json&id=' + serviceId,
                 headers: {'Accept-Language': $translate.proposedLanguage() || $translate.use()}
             });
         },
