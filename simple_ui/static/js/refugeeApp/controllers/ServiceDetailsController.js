c066a7591c17552b6e7261d27c7447ec2e3b5c92
dpiasecki <dpiasecki@soldevelo.com>
2017-07-24 17:13:17 +0200

    Show html for additional info

--- simple_ui/static/js/refugeeApp/controllers/ServiceDetailsController.js
+++ simple_ui/static/js/refugeeApp/controllers/ServiceDetailsController.js
@@ -2,7 +2,6 @@ angular.module('refugeeApp').controller('ServiceDetailsController', function ($s
     var vm = this;
     vm.slug = $stateParams.slug ? $stateParams.slug : $cookies.get('locationSlug');
     vm.service = service;
-    vm.service.description = $sce.trustAsHtml(vm.service.description);
     vm.serviceIcon = serviceIcon;
     vm.location = location;
     vm.serviceType = serviceType;
