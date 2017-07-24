9d8042d37823abd223cf98610658fd118e922c38
dpiaseckisoldevelo <dpiasecki@soldevelo.com>
2017-07-24 17:34:25 +0200

    Show html for additional info (#350)
    * Show html for additional info
    
    * Delete $sce

--- simple_ui/static/js/refugeeApp/controllers/ServiceDetailsController.js
+++ simple_ui/static/js/refugeeApp/controllers/ServiceDetailsController.js
@@ -1,8 +1,7 @@
-angular.module('refugeeApp').controller('ServiceDetailsController', function ($scope, $stateParams, service, serviceIcon, location, serviceType, $cookies, $sce) {
+angular.module('refugeeApp').controller('ServiceDetailsController', function ($scope, $stateParams, service, serviceIcon, location, serviceType, $cookies) {
     var vm = this;
     vm.slug = $stateParams.slug ? $stateParams.slug : $cookies.get('locationSlug');
     vm.service = service;
-    vm.service.description = $sce.trustAsHtml(vm.service.description);
     vm.serviceIcon = serviceIcon;
     vm.location = location;
     vm.serviceType = serviceType;
